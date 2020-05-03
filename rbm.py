fixed major bug: weight average computation; adding adversary

rbm.py
@@ -136,15 +136,21 @@ class RestrictedBoltzmannMachine:
         self.visibleLayer = miniFantasyBatch
         for _ in range(nCDSteps):
             visibleOut, hiddenOut = self.gibbsSample()
+        visibleModelMean, hiddenModelMean, weightModelMean = \
+                self.computeParameterMeans(visibleOut, hiddenOut)
         #store for possible use by adversary
-        self.visibleModelMean, self.hiddenModelMean, self.weightModelMean = \
-                                self.computeParameterMeans(visibleOut, hiddenOut)
-        return self.visibleModelMean, self.hiddenModelMean, self.weightModelMean, visibleOut
+        self.visibleModel = visibleOut
+        self.hiddenModel = hiddenOut
+        self.visibleModelMean = visibleModelMean
+        self.hiddenModelMean = hiddenModelMean
+        self.weightModelMean = weightModelMean
+        return visibleModelMean, hiddenModelMean, weightModelMean, visibleOut
 
     def computeParameterMeans(self, visible, hidden):
         visibleMean = visible.mean(axis=0)
         hiddenMean = hidden.mean(axis=0)
-        weightMean = visibleMean[..., :, None] * hiddenMean[..., None, :] * visible.shape[0]
+        weightMean = (visible[..., :, None] * hidden[..., None, :]).mean(axis=0)
+        #weightMean = visibleMean[..., :, None] * hiddenMean[..., None, :] * visible.shape[0]
         return visibleMean, hiddenMean, weightMean
 
     def updateParameters(self):
@@ -195,7 +201,6 @@ class RestrictedBoltzmannMachine:
         visibleGradient, hiddenGradient, weightGradient, newFantasy = \
             self.computePCDGradient(miniBatch, miniFantasyBatch, nCDSteps=nCDSteps,
                                     l1Coefficient=l1Coefficient, l2Coefficient=l2Coefficient)
-
         #hack to stop changing the *Step pointer; req'd for
         # current implementation of histograms of *Steps
         self.visibleStep += adams['visible'].computeAdamStep(visibleGradient) - self.visibleStep
@@ -212,6 +217,16 @@ class RestrictedBoltzmannMachine:
                                           self.weightStep.mean()))
         return newFantasy
 
+    def computeAdversaryGradient(self, adversary):
+        adversaryPredictions = adversary.predict(miniFantasyBatch)
+        adversaryPredictionVariation = adversaryPredictions - adversaryPredictions.mean()
+        visibleModelVariation = self.visibleModel - self.visibleModelMean
+        hiddenModelVariation = self.hiddenModel - self.hiddenModelMean
+        weightModelVariation = self.visibleModel[..., :, None] * self.hiddenModel[..., None, :] - self.weightModelMean
+        visibleGradient = (adversaryPredictionVariation[:, None] * visibleModelVariation).mean(axis=0)
+        hiddenGradient = (adversaryPredictionVariation[:, None] * hiddenModelVariation).mean(axis=0)
+        weightGradient = (adversaryPredictionVariation[:, None, None] * weightModelVariation).mean(axis=0)
+        return visibleGradient, hiddenGradient, weightGradient
 
     #
     #analysis methods

test_mnist.py
@@ -128,7 +128,8 @@ def main():
         learningRate = 1e-4
     elif gradientWalker == 'adam':
         #learningRate = 1e-4
-        learningRate = powerLawGenerator(1e-2, -0.1)
+        #learningRate = powerLawGenerator(1e-2, -0.1)
+        learningRate = powerLawGenerator(1e-3, -0.1)
         adams = dict(zip(['visible', 'hidden', 'weights'],
                          [Adam(stepSize=learningRate) for _ in range(3)]))
     else:
@@ -189,6 +190,8 @@ def main():
     historicalRBMs = []
     hiddenUnitActivations = []
     historicalFEs = []
+    trainSamplesForFE = getMiniBatchByLabel(trainImagesByLabel, miniBatchSize*10, rng)
+    testSamplesForFE = getMiniBatchByLabel(testImagesByLabel, miniBatchSize*10, rng)
     setupEndTime = time.time()
 
     if runTraining is True:
@@ -224,20 +227,18 @@ def main():
 
     outputStartTime = time.time()
     #plot reconstruction series
-    reconstructionPlots = []
-    reconstructionProbPlots = []
     visibleStarts = getMiniBatchByLabel(testImagesByLabel, 10, rng)
-    reconstructionPlots.append([visible] for visible in visibleStarts)
-    reconstructionProbPlots.append([visible] for visible in visibleStarts)
+    reconstructionPlots = [[visible] for visible in visibleStarts]
+    reconstructionProbPlots = [[visible] for visible in visibleStarts]
     for i, visible in enumerate(visibleStarts):
         rbm.visibleLayer = visible
         for _ in range(plotNumber-1):
             for _ in range(plotStride):
                 visible, _ = rbm.gibbsSample(hiddenUnitsStochastic=False)
-            reconstructionProbPlots.append(visible)
-            reconstructionPlots.append(rbm.rollBernoulliProbabilities(visible))
-        plotMNISTSeries(reconstructionProbPlots, fileName=''.join((mnistReconProbPlotFilePrefix, f'{i}.pdf')))
-        plotMNISTSeries(reconstructionPlots, fileName=''.join((mnistReconPlotFilePrefix, f'{i}.pdf')))
+            reconstructionProbPlots[i].append(visible)
+            reconstructionPlots[i].append(rbm.rollBernoulliProbabilities(visible))
+        plotMNISTSeries(reconstructionProbPlots[i], fileName=''.join((mnistReconProbPlotFilePrefix, f'{i}.pdf')))
+        plotMNISTSeries(reconstructionPlots[i], fileName=''.join((mnistReconPlotFilePrefix, f'{i}.pdf')))
 
     #plot parameter histograms
     print('#step\tparaLo\tparaHi\tgradLo\tgradHi\tratioLo\tratioHi')
@@ -248,18 +249,6 @@ def main():
             _, gradxs, gradys = gradientHistogramsByParameterType[parameterType][i]
             print('{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}'.format(gradxs.min(), gradxs.max(),
                                         gradxs.min()/xs.min(), gradxs.max()/xs.max()))
-
-            #parameterHistogramFileName = ''.join((parameterHistogramFilePrefix,
-            #                                      parameterType,
-            #                                      f'{step}.pdf'))
-            #diagnostics.plotHistogramArray(xs, ys, title=parameterType+f' {step}',
-            #                               fileName=parameterHistogramFileName)
-
-            #gradientHistogramFileName = ''.join((gradientHistogramFilePrefix,
-            #                                     parameterType,
-            #                                     f'{step}.pdf'))
-            #diagnostics.plotHistogramArray(gradxs, gradys, title=parameterType+f' grad {step}',
-            #                               fileName=gradientHistogramFileName)
 
         parameterHistogramTSFileName = ''.join((parameterHistogramFilePrefix,
                                                 parameterType,
@@ -293,11 +282,9 @@ def main():
                                                      fileName=hiddenUnitActivationFileName)
 
     #plot FE vs time
-    trainSamplesForFE = getMiniBatchByLabel(trainImagesByLabel, miniBatchSize*10, rng)
-    testSamplesForFE = getMiniBatchByLabel(testImagesByLabel, miniBatchSize*10, rng)
     t = [fe[0] for fe in historicalFEs]
-    trainFE = [fe[1] for fe in historicalFEs]
-    testFE = [fe[2] for fe in historicalFEs]
+    trainFE = np.array([fe[1] for fe in historicalFEs])
+    testFE = np.array([fe[2] for fe in historicalFEs])
     diagnostics.plotTrainingTestAverageFEVsTime(t, trainFE, testFE, fileName=feFileName)
     diagnostics.plotTrainingTestAverageFEVsTime(t, trainFE/testFE, None, fileName=feRatioFileName)
 